737408af5e2d6d9d538bb72bd35630b9e66de2ec
Daniel Micay <danielmicay@gmail.com>
2014-03-14 20:27:26 -0400

    improve the example

--- example.rs
+++ example.rs
@@ -1,23 +1,40 @@
 extern crate seccomp;
 
-use seccomp::{Filter, Compare, ACT_TRAP, ACT_ALLOW, OpEq, syscall};
+use seccomp::{Filter, Compare, ACT_KILL, ACT_ALLOW, OpEq, syscall};
 
 #[start]
 fn start(_argc: int, _argv: **u8) -> int {
     use std::libc::{c_void, size_t};
 
-    let filter = Filter::new(ACT_TRAP);
+    let outstr = bytes!("output for stdout\n");
+    let errstr = bytes!("output for stderr\n");
 
-    // write(1, x, y)
-    let stdout = Compare::new(0, OpEq, 1);
-    filter.rule_add(ACT_ALLOW, syscall::WRITE, [stdout]);
+    // set killing the process as the default handler, as we want a whitelist
+    let filter = Filter::new(ACT_KILL);
 
-    // exit_group(x)
-    filter.rule_add(ACT_ALLOW, syscall::EXIT_GROUP, []);
+    // allow `write(1, outstr.as_ptr(), outstr.len())`
+    filter.rule_add(ACT_ALLOW, syscall::WRITE, [
+        Compare::new(0, OpEq, 1),
+        Compare::new(1, OpEq, outstr.as_ptr() as u64),
+        Compare::new(2, OpEq, outstr.len() as u64)
+    ]);
 
+    // allow `write(2, errstr.as_ptr(), errstr.len())`
+    filter.rule_add(ACT_ALLOW, syscall::WRITE, [
+        Compare::new(0, OpEq, 2),
+        Compare::new(1, OpEq, errstr.as_ptr() as u64),
+        Compare::new(2, OpEq, errstr.len() as u64)
+    ]);
+
+    // allow `exit_group(0)`
+    filter.rule_add(ACT_ALLOW, syscall::EXIT_GROUP, [Compare::new(0, OpEq, 0)]);
+
+    // activate the filtering rules
     filter.load();
 
-    let s = bytes!("foobar\n");
-    unsafe { std::libc::write(1, s.as_ptr() as *c_void, s.len() as size_t); }
-    unsafe { std::libc::exit(0) }
+    unsafe {
+        std::libc::write(1, outstr.as_ptr() as *c_void, outstr.len() as size_t);
+        std::libc::write(2, errstr.as_ptr() as *c_void, errstr.len() as size_t);
+        std::libc::exit(0)
+    }
 }
